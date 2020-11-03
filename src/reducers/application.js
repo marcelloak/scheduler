2afc3d6fffaae3bbe7b86894926cbf2fcdc6f537
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-03 13:28:41 -0700

    Fixed bug with spots when editing appointment

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -18,10 +18,14 @@ export default function reducer(state, action) {
       }; 
   
       const days = [...state.days];
+
+      let spots = 0;
+      if (state.appointments[id].interview && !interview) spots = 1;
+      if (!state.appointments[id].interview && interview) spots = -1;
   
       for (let i = 0; i < days.length; i++) {
         if (days[i].appointments.includes(id)) {
-          days[i] = {...days[i], spots: days[i].spots + (interview ? -1 : 1)}
+          days[i] = {...days[i], spots: days[i].spots + spots}
         }
       }
       return {...state, appointments, days}

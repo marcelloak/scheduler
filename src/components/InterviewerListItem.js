226d90734e876bc8a5ae3e1d32f1fb6838c7e9da
Marcello Kuenzler <marcello.kuenzler@gmail.com>
2020-11-04 11:09:46 -0700

    Set unchanging variables to const

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -4,11 +4,11 @@ import classNames from 'classnames';
 import 'components/InterviewerListItem.scss'
 
 export default function InterviewerListItem(props) {
-  let interviewerListItemClass = classNames('interviewers__item', {
+  const interviewerListItemClass = classNames('interviewers__item', {
      'interviewers__item--selected': props.selected,
   });
 
-  let interviewerListItemImageClass = classNames('interviewers__item-image', {
+  const interviewerListItemImageClass = classNames('interviewers__item-image', {
     'interviewers__item-image--selected': props.selected,
  });
 
